0df48f17821a91975e54dc65826d9c77cb44eb16
Ben Gott <bengott@gmail.com>
2014-09-30 12:14:30 -0500

    Version bump to 0.1.0 - also specify jparker:crypto-md5 version in package.js - versions.json updated during publish

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "bengott:avatar",
   summary: "Consolidated user avatar template (twitter, facebook, gravatar, etc.)",
-  version: "0.0.1",
+  version: "0.1.0",
   git: "https://github.com/bengott/meteor-avatar"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3');
-  api.use(['templating', 'stylus', 'jparker:crypto-md5'], ['client']);
+  api.use(['templating', 'stylus', 'jparker:crypto-md5@0.1.1'], ['client']);
   api.addFiles(
     [
       'helpers.js',
