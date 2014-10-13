743c808c95656a0ae24c832b108bdaff97ee7556
Ben Gott <bengott@gmail.com>
2014-10-12 21:33:04 -0500

    Cleanup after merging PRs - renamed 'defaultAvatar' option to 'defaultAvatarUrl'

--- helpers.js
+++ helpers.js
@@ -5,56 +5,59 @@ Avatar = {
     // If defined (e.g. from a startup config file in your app), this property on the user object
     // will be used for retrieving gravatars (useful when user emails are not published) 
     emailHashProperty: '',
-    // If defined (e.g. from a startup config file in your app), this property will replace default avatar url
-    defaultAvatar: ''
+    // If defined (e.g. from a startup config file in your app), this will replace default avatar url
+    defaultAvatarUrl: ''
   },
 
-  // Get the url of the user's avatar, either from twitter, facebook, or gravatar (for now)
+  // Get the url of the user's avatar
   getUrl: function (user) {
+    var url;
     if (user) {
       var svc = getService(user);
       if (svc === 'twitter') {
         // use larger image (200x200 is smallest custom option)
-        return user.services.twitter.profile_image_url.replace('_normal.', '_200x200.');
+        url = user.services.twitter.profile_image_url.replace('_normal.', '_200x200.');
       }
       else if (svc === 'facebook') {
         // use larger image (~200x200)
-        return 'http://graph.facebook.com/' + user.services.facebook.id + '/picture?type=large';
+        url = 'http://graph.facebook.com/' + user.services.facebook.id + '/picture?type=large';
       }
       else if (svc === 'google') {        
-        return user.services.google.picture;
+        url = user.services.google.picture;
       }
       else if (svc === 'github') {
-        return 'http://avatars.githubusercontent.com/u/' + user.services.github.id + '?v=2';
+        url = 'http://avatars.githubusercontent.com/u/' + user.services.github.id + '?v=2';
       }
       else if (svc === 'instagram') {
-        return user.services.instagram.profile_picture;
+        url = user.services.instagram.profile_picture;
       }
       else if (svc === 'none') {
         var options = {
           s: 200, // use 200x200 like twitter and facebook above (might be useful later)
           secure: location.protocol === 'https:'
         };
-        return Gravatar.imageUrl(getEmailHash(user), options);
+        url = Gravatar.imageUrl(getEmailHash(user), options);
       }
     }
     // If all else fails, default image
-    if (Avatar.options.defaultAvatar) {
-      return Avatar.options.defaultAvatar;
+    if (Avatar.options.defaultAvatarUrl) {
+      url = Avatar.options.defaultAvatarUrl;
+    } else {
+      url = '/packages/bengott_avatar/default.png';
     }
-    return '/packages/bengott_avatar/default.png';
+    return url;
   }
 };
 
 // Get the account service to use for the user's avatar
-// Priority: Twitter > Facebook > (more to come...)
+// Priority: Twitter > Facebook > Google > GitHub > Instagram
 var getService = function (user) {
-  if      (user.services && user.services.twitter)  { return 'twitter'; }
-  else if (user.services && user.services.facebook) { return 'facebook'; }
-  else if (user.services && user.services.google) { return 'google'; }
-  else if (user.services && user.services.github) { return 'github'; }
+  if      (user.services && user.services.twitter)   { return 'twitter'; }
+  else if (user.services && user.services.facebook)  { return 'facebook'; }
+  else if (user.services && user.services.google)    { return 'google'; }
+  else if (user.services && user.services.github)    { return 'github'; }
   else if (user.services && user.services.instagram) { return 'instagram'; }
-  else                                              { return 'none'; }
+  else                                               { return 'none'; }
 };
 
 // Get the email hash, either from the specified emailHashProperty (pre-generated)
@@ -63,7 +66,8 @@ var getEmailHash = function (user) {
   var emailHash;
   if (Avatar.options.emailHashProperty) {
     emailHash = user[Avatar.options.emailHashProperty];
-  } else if (user.emails) {
+  }
+  else if (user.emails) {
     var email = user.emails[0].address; // TODO: try all emails
     emailHash = Gravatar.hash(email);
   }
