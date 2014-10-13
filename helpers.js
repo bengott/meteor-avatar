8e6ad0df82bcb582a461435a55326642ec43f052
Ben Gott <bengott@gmail.com>
2014-10-13 13:12:08 -0500

    Add support for Gravatar's default (d:) option

--- helpers.js
+++ helpers.js
@@ -1,17 +1,34 @@
 // Avatar object to be exported
 Avatar = {
   
+  // If defined (e.g. from a startup config file in your app), these options
+  // override default functionality
   options: {
-    // If defined (e.g. from a startup config file in your app), this property on the user object
-    // will be used for retrieving gravatars (useful when user emails are not published) 
+    
+    // This property on the user object will be used for retrieving gravatars
+    // (useful when user emails are not published)
     emailHashProperty: '',
-    // If defined (e.g. from a startup config file in your app), this will replace default avatar url
+    
+    // This will replace the standard default avatar URL. It can be a relative
+    // path (e.g. '/images/defaultAvatar.png')
     defaultAvatarUrl: ''
   },
 
   // Get the url of the user's avatar
   getUrl: function (user) {
-    var url;
+    
+    var url, defaultUrl;
+
+    if (Avatar.options.defaultAvatarUrl) {
+      defaultUrl = Avatar.options.defaultAvatarUrl;
+    } else {
+      defaultUrl = '/packages/bengott_avatar/default.png';
+    }
+    // If it's a relative path, complete the URL (prepend the origin)
+    if (defaultUrl.charAt(0) === '/' && defaultUrl.charAt(1) !== '/') {
+      defaultUrl = location.origin + defaultUrl;
+    }
+
     if (user) {
       var svc = getService(user);
       if (svc === 'twitter') {
@@ -32,19 +49,22 @@ Avatar = {
         url = user.services.instagram.profile_picture;
       }
       else if (svc === 'none') {
+        // NOTE: Gravatar's default (d:) option won't work when your app is running on localhost
+        // and you're using either the standard default URL or a custom options.defaultAvatarUrl
+        // that is a relative path (e.g. '/images/defaultAvatar.png'). If you'd like a temporary
+        // URL to use for dev purposes, you can uncomment the following line.
+        // defaultUrl = 'https://raw.githubusercontent.com/bengott/meteor-avatar/master/default.png'
         var options = {
+          d: defaultUrl,
           s: 200, // use 200x200 like twitter and facebook above (might be useful later)
           secure: location.protocol === 'https:'
         };
         url = Gravatar.imageUrl(getEmailHash(user), options);
       }
-    }
-    // If all else fails, default image
-    if (Avatar.options.defaultAvatarUrl) {
-      url = Avatar.options.defaultAvatarUrl;
     } else {
-      url = '/packages/bengott_avatar/default.png';
+      url = defaultUrl;
     }
+
     return url;
   }
 };
