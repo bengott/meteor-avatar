290895671f889ea3ecf7ffede2b5dfe40aaf319a
Ben Gott <bengott@gmail.com>
2014-10-13 22:39:36 -0500

    Update to Meteor 0.9.4

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "bengott:avatar",
   summary: "Consolidated user avatar template (twitter, facebook, gravatar, etc.)",
-  version: "0.1.2",
+  version: "0.1.3",
   git: "https://github.com/bengott/meteor-avatar"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.3');
+  api.versionsFrom('METEOR@0.9.4');
   api.use(['templating', 'underscore', 'stylus', 'jparker:crypto-md5@0.1.1'], ['client']);
   api.addFiles(
     [
